9db153640bcb20b0561699d5fd9828d42dd83ad6
blue <dheerajsinha042@gmail.com>
2026-02-02 21:12:38 +0530

    "dir issue fixed"

--- Modern-C/Chap_1-3/ISBN.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int gs1, ident, pub_code, item, checknum;
-    printf("Enter ISBN: \n");
-    scanf("%d-%d-%d-%d-%d", &gs1, &ident, &pub_code, &item, &checknum);
-    printf("GS1 prefix:%d\nGroup identifier:%d\nPublisher code:%d\nItem number:%d\nCheck digit:%d\n",gs1, ident, pub_code, item, checknum);
-    return 0;
-}
--- Modern-C/Chap_1-3/scanf.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(){
-printf("%10e",.12);
-return 0;}
